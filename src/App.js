48d030842eb008393537b39079af4ca855a0b5e7
alexpandian23 <smartalex125@gmail.com>
2024-02-29 18:08:18 +0530

    changes in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { Login } from './logintask/Login';
+import { Signup } from './logintask/Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My first Project
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Signup' element={<Signup />} />
+
+        </Routes>
+      </BrowserRouter>
+
      
     </div>
   );
